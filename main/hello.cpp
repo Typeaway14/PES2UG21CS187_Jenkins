ed214f6c653b60e2ca7f318bbc970e3fbbc0a963
Typeaway14 <harsh.som33@gmail.com>
2024-03-12 19:45:01 +0530

    3nd commit to test webhook

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "Hello, Hi very cool\n";
+  cout << "Hello, Hi trial 2 very cool\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
